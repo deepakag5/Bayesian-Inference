78c6257740408a01288d5c6a8b219c59b50aac45
iamdeepaka <deepakagarwal@gwu.edu>
2017-11-12 14:02:08 -0500

    Density Plot and Pair Plot for SalePrice

--- ames_dataset.R
+++ ames_dataset.R
@@ -1,6 +1,6 @@
 ## Load and install the required libraries
 
-load.libraries <- c('data.table', 'testthat', 'gridExtra', 'corrplot', 'GGally', 'ggplot2', 'e1071', 'dplyr')
+load.libraries <- c('data.table', 'testthat', 'gridExtra', 'corrplot', 'GGally', 'ggplot2', 'e1071', 'dplyr','reshape2')
 install.lib <- load.libraries[!load.libraries %in% installed.packages()]
 
 for(libs in install.lib) install.packages(libs)
@@ -24,8 +24,32 @@ numeric_var <- names(train)[which(sapply(train, is.numeric))]
 
 str(train)
 
+## First we would like to see the distribution of Response Variable - SalePrice
+
+hist(train$SalePrice, # histogram
+     col="skyblue", # column color
+     border="black",
+     prob = TRUE, # show densities instead of frequencies
+     xlab = "SalePrice",
+     main = "SalePrice Distribution")
+lines(density(train$SalePrice), # density plot
+      lwd = 2, # thickness of line
+      col = "blue")
+
+
+
+
 ## Check for the relationship of the response variable with Numerical Variables
-## We would create a pairplot of Sales Price with all numeric variables
+## We would create pairplot of Sales Price with all numeric variables
+
+train_numeric <- train[,numeric_var]
+
+train_numeric_melt <- melt(train_numeric, id="SalePrice")
+
+ggplot(train_numeric_melt,aes(x=value, y=SalePrice)) +
+  facet_wrap(~variable, scales = "free")+
+  geom_point()
+
 
 ## Check number of NA in all the columns
 
